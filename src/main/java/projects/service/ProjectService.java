db59f35a63f95bdc24944cc073ad811bbb99069a
hmackowski <hmackowski@gmail.com>
2022-11-19 16:24:21 -0500

    fist commit

---
+++ src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package projects.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import projects.dao.ProjectDao;
+import projects.entity.Project;
+import projects.exception.DbException;
+
+/**
+ * @author hmack
+ *
+ */
+public class ProjectService {
+
+	private ProjectDao projectDao = new ProjectDao();
+
+	/**
+	 * @param project
+	 * @return
+	 */
+	public Project addProject(Project project) {
+
+		return projectDao.insertProject(project);
+	}
+
+	/**
+	 * @return
+	 */
+	public List<Project> fetchAllProjects() {
+		return projectDao.fetchAllProjects();
+	}
+
+	/**
+	 * @param projectId
+	 * @return
+	 */
+	public Project fetchProjectById(Integer projectId) {
+		return projectDao.fetchProjectsById(projectId).orElseThrow(
+				() -> new NoSuchElementException("Project with project ID=" + projectId + " does not exist."));
+	}
+
+	/**
+	 * @param project
+	 */
+	public void modifyProjectDetails(Project project) {
+		if(!projectDao.modifyProjectDetails(project)) {
+			throw new DbException("Project with ID=" + project.getProjectId() + " does not exist.");
+		}
+		
+	}
+
+	/**
+	 * @param projectId
+	 */
+	public void deleteProject(Integer projectId) {
+		if(!projectDao.deleteProject(projectId)) {
+			throw new DbException("Project with ID=" + projectId + " does not exist.");
+		}
+		
+	}
+
+}
